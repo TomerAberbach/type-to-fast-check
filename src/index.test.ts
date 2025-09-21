82d26cc4763d2a0a79baf1662bb042276ead4c77
Tomer Aberbach <tomer@aberba.ch>
2025-09-21 00:42:26 -0400

    feat: enums + test refactor (#6)

--- src/index.test.ts
+++ src/index.test.ts
@@ -1,212 +1,280 @@
 import { fc, test } from '@fast-check/vitest'
+import ts from 'typescript'
 import { expect } from 'vitest'
-import typeToArbitrary from './index.js'
+import { FAST_CHECK_IDENTIFIER_NAME } from './fast-check.ts'
+import createTransformer from './transformer.ts'
 
 test.each([
   {
-    name: `any`,
-    typeToArbitrary: () => typeToArbitrary<any>(),
+    type: `any`,
     expectedArb: fc.anything(),
   },
   {
-    name: `unknown`,
-    typeToArbitrary: () => typeToArbitrary<unknown>(),
+    type: `unknown`,
     expectedArb: fc.anything(),
   },
   {
-    name: `void`,
-    // eslint-disable-next-line typescript/no-invalid-void-type
-    typeToArbitrary: () => typeToArbitrary<void>(),
+    type: `void`,
     expectedArb: fc.constant(undefined),
   },
   {
-    name: `undefined`,
-    typeToArbitrary: () => typeToArbitrary<undefined>(),
+    type: `undefined`,
     expectedArb: fc.constant(undefined),
   },
   {
-    name: `boolean`,
-    typeToArbitrary: () => typeToArbitrary<boolean>(),
+    type: `boolean`,
     expectedArb: fc.boolean(),
   },
   {
-    name: `false`,
-    typeToArbitrary: () => typeToArbitrary<false>(),
+    type: `false`,
     expectedArb: fc.constant(false),
   },
   {
-    name: `true`,
-    typeToArbitrary: () => typeToArbitrary<true>(),
+    type: `true`,
     expectedArb: fc.constant(true),
   },
   {
-    name: `number`,
-    typeToArbitrary: () => typeToArbitrary<number>(),
+    type: `number`,
     expectedArb: fc.double(),
   },
   {
-    name: `integer literal`,
-    typeToArbitrary: () => typeToArbitrary<1>(),
+    type: `1`,
     expectedArb: fc.constant(1),
   },
   {
-    name: `decimal literal`,
-    typeToArbitrary: () => typeToArbitrary<1.5>(),
+    type: `1.5`,
     expectedArb: fc.constant(1.5),
   },
   {
-    name: `bigint`,
-    typeToArbitrary: () => typeToArbitrary<bigint>(),
+    type: `bigint`,
     expectedArb: fc.bigInt(),
   },
   {
-    name: `bigint literal`,
-    typeToArbitrary: () => typeToArbitrary<1n>(),
+    type: `1n`,
     expectedArb: fc.constant(1n),
   },
   {
-    name: `string`,
-    typeToArbitrary: () => typeToArbitrary<string>(),
+    type: `string`,
     expectedArb: fc.string(),
   },
   {
-    name: `string literal`,
-    // eslint-disable-next-line stylistic/quotes
-    typeToArbitrary: () => typeToArbitrary<'Hello World!'>(),
+    type: `'Hello World!'`,
     expectedArb: fc.constant(`Hello World!`),
   },
   {
-    name: `symbol`,
-    typeToArbitrary: () => typeToArbitrary<symbol>(),
+    type: `symbol`,
     expectedArb: fc.string().map(Symbol),
     stringify: true,
   },
   {
-    name: `array`,
-    typeToArbitrary: () => typeToArbitrary<string[]>(),
+    type: `string[]`,
     expectedArb: fc.array(fc.string()),
   },
   {
-    name: `array interface`,
-    // eslint-disable-next-line typescript/array-type
-    typeToArbitrary: () => typeToArbitrary<Array<string>>(),
+    type: `Array<string>`,
     expectedArb: fc.array(fc.string()),
   },
   {
-    name: `readonly array`,
-    typeToArbitrary: () => typeToArbitrary<readonly string[]>(),
+    type: `readonly string[]`,
     expectedArb: fc.array(fc.string()),
   },
   {
-    name: `readonly array interface`,
-    // eslint-disable-next-line typescript/array-type
-    typeToArbitrary: () => typeToArbitrary<ReadonlyArray<string>>(),
+    type: `ReadonlyArray<string>`,
     expectedArb: fc.array(fc.string()),
   },
   {
-    name: `object`,
-    typeToArbitrary: () => typeToArbitrary<object>(),
-    expectedArb: fc.object(),
-  },
-  {
-    name: `object literal`,
-    typeToArbitrary: () => typeToArbitrary<{ a: string; b: number }>(),
+    type: `{ a: string; b: number }`,
     expectedArb: fc.record({ a: fc.string(), b: fc.double() }),
   },
   {
-    name: `object literal with undefined`,
-    typeToArbitrary: () =>
-      typeToArbitrary<{ a: string; b: number | undefined }>(),
+    type: `{ a: string; b: number | undefined }`,
     expectedArb: fc.record({
       a: fc.string(),
       b: fc.option(fc.double(), { nil: undefined }),
     }),
   },
   {
-    name: `object literal with optionals`,
-    typeToArbitrary: () => typeToArbitrary<{ a: string; b?: number }>(),
+    type: `{ a: string; b?: number }`,
     expectedArb: fc.record(
       { a: fc.string(), b: fc.option(fc.double(), { nil: undefined }) },
       { requiredKeys: [`a`] },
     ),
   },
   {
-    name: `union of false and true`,
-    typeToArbitrary: () => typeToArbitrary<false | true>(),
+    type: `object`,
+    expectedArb: fc.object(),
+  },
+  {
+    setUp: `
+      enum StringEnum {
+        B = 'b',
+        A = 'a',
+        C = 'c',
+      }
+    `,
+    type: `StringEnum`,
+    expectedArb: fc.constantFrom(`a`, `b`, `c`),
+  },
+  {
+    setUp: `
+      enum IntEnum {
+        B = 1,
+        A = 0,
+        C = 2,
+      }
+    `,
+    type: `IntEnum`,
+    expectedArb: fc.constantFrom(0, 1, 2),
+  },
+  {
+    setUp: `
+      enum ImplicitIntEnum {
+        A,
+        B,
+        C,
+      }
+    `,
+    type: `ImplicitIntEnum`,
+    expectedArb: fc.constantFrom(0, 1, 2),
+  },
+  {
+    setUp: `
+      enum PartiallyImplicitIntEnum {
+        A = 4,
+        B,
+        C = 2,
+        D,
+      }
+    `,
+    type: `PartiallyImplicitIntEnum`,
+    expectedArb: fc.constantFrom(2, 3, 4, 5),
+  },
+  {
+    type: `false | true`,
     expectedArb: fc.boolean(),
   },
   {
-    name: `union of true and false`,
-    typeToArbitrary: () => typeToArbitrary<true | false>(),
+    type: `true | false`,
     expectedArb: fc.boolean(),
   },
   {
-    name: `union of integer constants`,
-    typeToArbitrary: () => typeToArbitrary<1 | 3 | 2 | 4>(),
+    type: `1 | 3 | 2 | 4`,
     expectedArb: fc.constantFrom(1, 2, 3, 4),
   },
   {
-    name: `union of bigint constants`,
-    typeToArbitrary: () => typeToArbitrary<1n | 3n | 2n | 4n>(),
+    type: `1n | 3n | 2n | 4n`,
     expectedArb: fc.constantFrom(1n, 2n, 3n, 4n),
   },
   {
-    name: `union of string constants`,
-    // eslint-disable-next-line stylistic/quotes
-    typeToArbitrary: () => typeToArbitrary<'b' | 'a' | 'd' | 'c'>(),
+    type: `'b' | 'a' | 'd' | 'c'`,
     expectedArb: fc.constantFrom(`a`, `b`, `c`, `d`),
   },
   {
-    name: `union of string and undefined`,
-    typeToArbitrary: () => typeToArbitrary<string | undefined>(),
+    type: `string | undefined`,
     expectedArb: fc.option(fc.string(), { nil: undefined }),
   },
   {
-    name: `union of string and null`,
-    typeToArbitrary: () => typeToArbitrary<string | null>(),
+    type: `string | null`,
     expectedArb: fc.option(fc.string()),
   },
   {
-    name: `union of string, undefined, and null`,
-    typeToArbitrary: () => typeToArbitrary<string | undefined | null>(),
+    type: `string | undefined | null`,
     expectedArb: fc.oneof(fc.string(), fc.constantFrom(null, undefined)),
   },
   {
-    name: `union of string and number`,
-    typeToArbitrary: () => typeToArbitrary<string | number>(),
+    type: `string | number`,
     expectedArb: fc.oneof(fc.string(), fc.double()),
   },
   {
-    name: `unconstrained type parameter`,
-    typeToArbitrary: <T>() => typeToArbitrary<T>(),
+    wrapArb: (code: string) => `
+      (<UnconstrainedTypeParam>() => {
+        ${code}
+      })()
+    `,
+    type: `UnconstrainedTypeParam`,
     expectedArb: fc.anything(),
   },
   {
-    name: `constrained type parameter`,
-    typeToArbitrary: <T extends string>() => typeToArbitrary<T>(),
+    wrapArb: (code: string) => `
+      (<ConstrainedTypeParam extends string>() => {
+        ${code}
+      })()`,
+    type: `ConstrainedTypeParam`,
     expectedArb: fc.string(),
   },
-  // {
-  //   name: `this`,
-  //   typeToArbitrary: () => {
-  //     class Class {
-  //       // eslint-disable-next-line typescript/class-methods-use-this
-  //       public arb() {
-  //         return typeToArbitrary<this>()
-  //       }
-  //     }
-  //     return new Class().arb()
-  //   },
-  //   expectedArb: fc.string(),
-  // },
 ])(
-  `typeToArbitrary transforms $name types`,
-  ({ typeToArbitrary, expectedArb, stringify }) => {
-    const arb = typeToArbitrary()
+  `typeToArbitrary transforms $type`,
+  ({ setUp = ``, wrapArb = code => code, type, expectedArb, stringify }) => {
+    const arb = typeToArbitrary(setUp, wrapArb, type)
 
     expect(sample(arb, stringify)).toStrictEqual(sample(expectedArb, stringify))
   },
 )
+
+const typeToArbitrary = (
+  setUp: string,
+  wrapArb: (code: string) => string,
+  type: string,
+) => {
+  const sourceCode = [setUp, wrapArb(`const arb = typeToArbitrary<${type}>()`)]
+    .filter(Boolean)
+    .join(`\n\n`)
+
+  const transformedCode = transformTypeScript(sourceCode).replaceAll(
+    FAST_CHECK_IDENTIFIER_NAME,
+    `fc`,
+  )
+  const arbCodeRegExp = /const arb =(?<arb>.*)/u
+  const arbCode = arbCodeRegExp.exec(transformedCode)?.groups?.arb
+
+  // eslint-disable-next-line typescript/no-implied-eval, no-new-func
+  const arbitraryFunction = new Function(`fc`, `return ${arbCode}`) as (
+    fastCheck: typeof fc,
+  ) => fc.Arbitrary<unknown>
+  return arbitraryFunction(fc)
+}
+
+const transformTypeScript = (code: string): string => {
+  const fileName = `test.ts`
+  const sourceFile = ts.createSourceFile(
+    fileName,
+    code,
+    ts.ScriptTarget.ESNext,
+    true,
+  )
+  const currentCompilerHost: ts.CompilerHost = {
+    ...compilerHost,
+    getSourceFile: (name, ...args) =>
+      name === fileName
+        ? sourceFile
+        : compilerHost.getSourceFile.call(currentCompilerHost, name, ...args),
+  }
+  const program = ts.createProgram(
+    [fileName],
+    compilerOptions,
+    currentCompilerHost,
+  )
+
+  const result = ts.transform(
+    sourceFile,
+    [createTransformer(program)],
+    compilerOptions,
+  )
+  try {
+    return printer.printFile(result.transformed[0]!)
+  } finally {
+    result.dispose()
+  }
+}
+
+const compilerOptions: ts.CompilerOptions = {
+  target: ts.ScriptTarget.ESNext,
+  module: ts.ModuleKind.Preserve,
+  strict: true,
+  lib: [`lib.esnext.full.d.ts`],
+}
+const compilerHost = ts.createCompilerHost(compilerOptions)
+const printer = ts.createPrinter()
 
 const sample = <T>(arb: fc.Arbitrary<T>, stringify = false): unknown[] => {
   const samples = fc.sample(arb, { seed: 42, numRuns: 5 })
--- vitest.config.ts
+++ vitest.config.ts
@@ -1,4 +1,3 @@
-import typescript from '@rollup/plugin-typescript'
 import { defaultExclude, defineConfig } from 'vitest/config'
 
 export default defineConfig({
@@ -9,19 +8,5 @@ export default defineConfig({
       include: [`src`],
     },
     exclude: [...defaultExclude, `.rollup.cache`],
-    watch: false,
   },
-  plugins: [
-    typescript({
-      tsconfig: `./tsconfig.json`,
-      compilerOptions: {
-        plugins: [
-          {
-            transform: `./dist/transformer.js`,
-            type: `program`,
-          },
-        ],
-      },
-    }),
-  ],
 })
